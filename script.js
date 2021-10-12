2344370c8fe6a71688a49b8bc54394e9a7e2b9af
alfredochs <chanta.alfredo@gmail.com>
2021-10-12 15:56:04 +0200

    html css bg

---
+++ script.js
@@ -0,0 +1,10 @@
+const containerTabella = document.getElementById("container-tabella");
+
+for (let i = 1; i <= 7; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row", "g-0", "bg-danger");
+
+    for (let x = 1; x <= 100; x++) {
+
+    }
+}
