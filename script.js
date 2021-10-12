df3d929bb57ec762d1c5dcd2c0f214aa53b52ac8
alfredochs <chanta.alfredo@gmail.com>
2021-10-12 19:04:19 +0200

    exercise  done

--- script.js
+++ script.js
@@ -1,39 +1,27 @@
 const containerTabella = document.getElementById("container-tabella");
+const row = document.createElement("div");
+row.classList.add("row", "my-row-cols-7");
+containerTabella.append(row);
+for (i = 1; i <= 100; i++) {
+    const myBox = document.createElement("div");
+    myBox.classList.add("ratio", "ratio-1x1", "cella-normale");
+    myBox.textContent = i;
+    row.append(myBox);
 
-for (let i = 1; i <= 4; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row", "m-2");
-    containerTabella.append(row);
+    const multiploDi3 = i % 3 === 0;
+    const multiploDi5 = i % 5 === 0;
 
-    for (let x = 1; x <= 7; x++) {
-        const multiploDi3 = x % 3 === 0;
-        const multiploDi5 = x % 5 === 0;
-        const multiploDiEntrambi = multiploDi3 === multiploDi5;
-
-        const cell = document.createElement("div");
-        cell.classList.add("ratio", "ratio-1x1", "border");
-
-        let bgColor = "";
-
-
-
-        if (!multiploDi3 || !multiploDi5 || multiploDiEntrambi) {
-            bgColor = "cella-normale";
-        }
-
-        if (multiploDi3) {
-            bgColor = "multiplo3";
-        }
-        if (multiploDi5) {
-            bgColor = "multiplo5";
-        }
-        if ((multiploDi3 && multiploDi5)) {
-            bgColor = "multiplo3-5";
-        }
-        row.innerHTML += `<div class="col">
-                            <div class="ratio ratio-1x1 border ${bgColor}">
-                            <div></div>
-                            </div>
-                        </div>`;
+    if (multiploDi3) {
+        myBox.textContent = "fizz";
+        myBox.classList.add("multiplo3");
     }
-}
+    if (multiploDi5) {
+        myBox.classList.add("multiplo5");
+        myBox.textContent = "buzz";
+    }
+    if (multiploDi3 && multiploDi5) {
+        myBox.classList.add("multiplo3-5");
+        myBox.textContent = "fizzbuzz";
+
+    }
+};
